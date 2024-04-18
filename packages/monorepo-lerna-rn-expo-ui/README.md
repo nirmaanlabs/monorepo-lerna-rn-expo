96bd4c55738c9ce75930db2f72e9c20b153858e4
GAURAV YADAV <gaurav.gydv@gmail.com>
2024-04-18 18:49:15 +0530

    the example

---
+++ packages/monorepo-lerna-rn-expo-ui/README.md
@@ -0,0 +1,11 @@
+# `monorepo-lerna-rn-expo-ui`
+
+> TODO: description
+
+## Usage
+
+```
+const monorepoLernaRnExpoUi = require('monorepo-lerna-rn-expo-ui');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/monorepo-lerna-rn-expo-ui/__tests__/monorepo-lerna-rn-expo-ui.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const monorepoLernaRnExpoUi = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(monorepoLernaRnExpoUi(), 'Hello from monorepoLernaRnExpoUi');
+console.info('monorepoLernaRnExpoUi tests passed');
